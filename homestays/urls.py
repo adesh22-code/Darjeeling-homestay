251974c7084392cbf9b25a369dc287e4b29f27c8
adesh22-code <144451382+adesh22-code@users.noreply.github.com>
2026-06-29 11:48:16 +0530

    Update urls.py

--- homestays/urls.py
+++ homestays/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from .views import home
+from . import owner_views
 from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("homestay/<int:id>/", views.homestay_detail, name="homestay_detail"),
     path("my-bookings/", views.my_bookings, name="my_bookings"),
+    path(
+    "owner/dashboard/",
+    owner_views.owner_dashboard,
+    name="owner_dashboard",
+    ),
 ]
